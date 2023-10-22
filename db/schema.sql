8ac6da01f4f7d89d2e620a02566ff17d68753c89
Misachka <136197530+Misachka@users.noreply.github.com>
2023-10-22 13:16:01 -0400

    updated schema

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+-- Use the employees_db 
+USE employees_db;
+
+
+-- Create a table name department, 2 rows
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL
+);
+
+-- Create a table named role, 4 rows
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY(department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+-- Create a table name employee, 5 rows
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY(role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+);
